ffab5a56de1d64995487df9c0add8406c259fa61
Chris I <christopherisler1@gmail.com>
2024-10-27 06:23:48 -0500

    introduce router

--- ChimeCore/Program.cs
+++ ChimeCore/Program.cs
@@ -1,8 +1,7 @@
 using ChimeCore.Data;
-using ChimeCore.Models;
+using ChimeCore.Routes;
 using Microsoft.EntityFrameworkCore;
 using Azure.Storage.Blobs;
-using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -70,6 +69,8 @@ app.UseAntiforgery();
 
 app.UseHttpsRedirection();
 
+app.MapChimesRoutes();
+
 app.MapGet("/health-check", () =>
 {
     return TypedResults.Ok("hello world!");
@@ -77,96 +78,5 @@ app.MapGet("/health-check", () =>
 .WithName("HealthCheck")
 .WithOpenApi();
 
-var chimesRouter = app.MapGroup("/Chimes").WithOpenApi().DisableAntiforgery();
-
-chimesRouter.MapPost("/", CreateChime).WithName("CreateChime");
-chimesRouter.MapGet("/", GetAllChimes).WithName("GetAllChimes");
-chimesRouter.MapGet("/{id}", GetChimeById).WithName("GetChimeById");
-chimesRouter.MapPut("/{id}", UpdateChime).WithName("UpdateChime");
-chimesRouter.MapDelete("/{id}", DeleteChime).WithName("DeleteChime");
-
-/* chimesRouter.MapGet("/{username}", async (string username, ApplicationDbContext ctx) => */
-/*     await ctx.Chimes.Where(_ => _.By.ToLower().Contains(username.ToLower())).ToListAsync()); */
-
-static async Task<IResult> GetAllChimes(ApplicationDbContext ctx)
-{
-    return TypedResults.Ok(
-        await ctx.Chimes.Where(_ => _.Deleted == false).OrderByDescending(_ => _.Time).ToListAsync()
-    );
-}
-
-static async Task<IResult> CreateChime(
-    [FromForm] string by,
-    [FromForm] int byId,
-    [FromForm] string text,
-    IFormFile? file,
-    ApplicationDbContext ctx,
-    BlobServiceClient blobServiceClient,
-    IConfiguration cfg,
-    CancellationToken cancellationToken
-)
-{
-    string? mediaUrl = null;
-
-    // upload and get shareable url from azure storage
-    if (file != null)
-    {
-        var containerName = cfg["AzureBlobStorage:ContainerName"];
-        var blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
-
-        await blobContainerClient.CreateIfNotExistsAsync();
-
-        var blobClient = blobContainerClient.GetBlobClient(file.FileName);
-
-        await blobClient.UploadAsync(file.OpenReadStream(), overwrite: true, cancellationToken);
-
-        mediaUrl = blobClient.Uri.ToString();
-    }
-
-    var chime = new Chime(by, byId, text, kids: [], mediaUrl);
-
-    ctx.Chimes.Add(chime);
-    await ctx.SaveChangesAsync();
-
-    return TypedResults.Created($"/Chime/{chime.Id}", chime);
-}
-
-static async Task<IResult> GetChimeById(int id, ApplicationDbContext ctx)
-{
-    return await ctx.Chimes.Where(_ => _.Deleted == false && _.Id == id).FirstOrDefaultAsync() is Chime chime
-        ? TypedResults.Ok(chime)
-        : TypedResults.NotFound("Failed to find Item with ID: " + id);
-}
-
-static async Task<IResult> UpdateChime(int id, ChimeDTO chimeDTO, ApplicationDbContext ctx)
-{
-    var chime = await ctx.Chimes.SingleOrDefaultAsync(_ => _.Id == id && _.Deleted == false);
-    if (chime is null)
-    {
-        return TypedResults.NotFound("Failed to find Item with ID: " + id);
-    }
-
-    chime.Text = chimeDTO.Text;
-    chime.Kids = chimeDTO.Kids;
-
-    await ctx.SaveChangesAsync();
-
-    return TypedResults.Ok(chime);
-}
-
-static async Task<IResult> DeleteChime(int id, ApplicationDbContext ctx)
-{
-    var chime = await ctx.Chimes.SingleOrDefaultAsync(_ => _.Id == id && _.Deleted == false);
-    if (chime is null)
-    {
-        return TypedResults.NotFound("Failed to find Item with ID: " + id);
-    }
-
-    chime.Deleted = true;
-
-    await ctx.SaveChangesAsync();
-
-    return TypedResults.NoContent();
-}
 
 app.Run();
---
+++ ChimeCore/Routes/Chimes.cs
@@ -0,0 +1,107 @@
+using ChimeCore.Models;
+using Microsoft.EntityFrameworkCore;
+using ChimeCore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Azure.Storage.Blobs;
+
+namespace ChimeCore.Routes
+{
+    public static class ChimesRoutes
+    {
+        public static void MapChimesRoutes(this IEndpointRouteBuilder app)
+        {
+            var chimesRouter = app.MapGroup("/Chimes").WithOpenApi().DisableAntiforgery();
+
+            chimesRouter.MapPost("/", CreateChime).WithName("CreateChime");
+            chimesRouter.MapGet("/", GetAllChimes).WithName("GetAllChimes");
+            chimesRouter.MapGet("/{id}", GetChimeById).WithName("GetChimeById");
+            chimesRouter.MapPut("/{id}", UpdateChime).WithName("UpdateChime");
+            chimesRouter.MapDelete("/{id}", DeleteChime).WithName("DeleteChime");
+
+            /* chimesRouter.MapGet("/{username}", async (string username, ApplicationDbContext ctx) => */
+            /*     await ctx.Chimes.Where(_ => _.By.ToLower().Contains(username.ToLower())).ToListAsync()); */
+
+            static async Task<IResult> GetAllChimes(ApplicationDbContext ctx)
+            {
+                return TypedResults.Ok(
+                    await ctx.Chimes.Where(_ => _.Deleted == false).OrderByDescending(_ => _.Time).ToListAsync()
+                );
+            }
+
+            static async Task<IResult> CreateChime(
+                [FromForm] string by,
+                [FromForm] int byId,
+                [FromForm] string text,
+                IFormFile? file,
+                ApplicationDbContext ctx,
+                BlobServiceClient blobServiceClient,
+                IConfiguration cfg,
+                CancellationToken cancellationToken
+            )
+            {
+                string? mediaUrl = null;
+
+                // upload and get shareable url from azure storage
+                if (file != null)
+                {
+                    var containerName = cfg["AzureBlobStorage:ContainerName"];
+                    var blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+                    await blobContainerClient.CreateIfNotExistsAsync();
+
+                    var blobClient = blobContainerClient.GetBlobClient(file.FileName);
+
+                    await blobClient.UploadAsync(file.OpenReadStream(), overwrite: true, cancellationToken);
+
+                    mediaUrl = blobClient.Uri.ToString();
+                }
+
+                var chime = new Chime(by, byId, text, kids: [], mediaUrl);
+
+                ctx.Chimes.Add(chime);
+                await ctx.SaveChangesAsync();
+
+                return TypedResults.Created($"/Chime/{chime.Id}", chime);
+            }
+
+            static async Task<IResult> GetChimeById(int id, ApplicationDbContext ctx)
+            {
+                return await ctx.Chimes.Where(_ => _.Deleted == false && _.Id == id).FirstOrDefaultAsync() is Chime chime
+                    ? TypedResults.Ok(chime)
+                    : TypedResults.NotFound("Failed to find Item with ID: " + id);
+            }
+
+            static async Task<IResult> UpdateChime(int id, ChimeDTO chimeDTO, ApplicationDbContext ctx)
+            {
+                var chime = await ctx.Chimes.SingleOrDefaultAsync(_ => _.Id == id && _.Deleted == false);
+                if (chime is null)
+                {
+                    return TypedResults.NotFound("Failed to find Item with ID: " + id);
+                }
+
+                chime.Text = chimeDTO.Text;
+                chime.Kids = chimeDTO.Kids;
+
+                await ctx.SaveChangesAsync();
+
+                return TypedResults.Ok(chime);
+            }
+
+            static async Task<IResult> DeleteChime(int id, ApplicationDbContext ctx)
+            {
+                var chime = await ctx.Chimes.SingleOrDefaultAsync(_ => _.Id == id && _.Deleted == false);
+                if (chime is null)
+                {
+                    return TypedResults.NotFound("Failed to find Item with ID: " + id);
+                }
+
+                chime.Deleted = true;
+
+                await ctx.SaveChangesAsync();
+
+                return TypedResults.NoContent();
+            }
+
+        }
+    }
+}
